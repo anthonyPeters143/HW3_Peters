1d11f53671eeb765bcbb58bdf7a881e96148629b
APeters143 <anthonypeters@delta.edu>
2022-07-18 12:02:29 -0400

    NEEDS formating, ObserList Fixed, Handlers for buttons and ComBoxes

--- src/HW3_Peters.java
+++ src/HW3_Peters.java
@@ -1,6 +1,11 @@
+//package HW3_Peters;
+
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.fxml.*;
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
@@ -8,17 +13,39 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.TilePane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class HW3_Peters extends Application {
+    // Item array list
+    static ArrayList<Item> itemArrayList;
+
+    /// Item observable list
+    static ObservableList<Item> itemObservableList;
+
+    // Item ComboBox
+    static ComboBox<Item> itemComBox;
+
+    // Scenes
+    Scene addItemScene,deleteItemScene,modifyItemScene,dataAltScene,saleScene,mainScene;
+
+    // Keys
+    static String   FILE_NAME_KEY   = "item.txt",
+                    MAIN_STYLE      = "MainStyle.css";
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        //
-        String MAIN_TITLE           = "POST Register",
+
+        String  TYPE_HERE_DEFAULT   = "Type here...",
+
+                MAIN_TITLE          = "POST Register",
                 EOD_TOTAL_TITLE     = "Total sale for the day is: $",
 
                 ADD_BUTTON_TITLE    = "Add",
@@ -51,70 +78,203 @@ public class HW3_Peters extends Application {
                 ADD_ITEM_BUTTON_TITLE   = "Add Item",
                 DELETE_ITEM_BUTTON_TITLE= "Delete Item",
                 MOD_ITEM_BUTTON_TITLE   = "Modify Item",
-                QUIT_BUTTON_TITLE       = "Quit";
+                QUIT_BUTTON_TITLE       = "Quit",
 
+                ADD_ITEM_TITLE    = "Add Item",
+                DELETE_ITEM_TITLE = "Delete Item",
+                MODIFY_ITEM_TITLE = "Modify Item";
+        ;
 
         double eodTotalPrice = 0;
 
-        // Set up FXML and define objects in it
-        FXMLLoader fxmlPOSTLoader = new FXMLLoader(HW3_Peters.class.getResource("POST.fxml"));
+        // Initializing itemComBox from file input
+        Initializing();
+
+        // Create itemComBox node
+        TilePane itemSaleComBoxTilePane = new TilePane(itemComBox);
+
+        TilePane itemAltComBoxTilePane = new TilePane(itemComBox);
+
+        // Add scene
+        // Create add item scene title
+        Label addItemAltSceneTitle = new Label(ADD_ITEM_TITLE);
+
+        // Create item title/field HBox nodes
+        Label itemAltCodeTitle = new Label(ITEM_NAME_TITLE);
+        TextField itemAltCodeField = new TextField();
+        HBox itemAltCodeHB = new HBox(itemAltCodeTitle,itemAltCodeField);
+
+        Label itemAltNameTitle = new Label(ITEM_PRICE_TITLE);
+        TextField itemAltNameField = new TextField();
+        HBox itemAltNameHB = new HBox(itemAltNameTitle,itemAltNameField);
+
+        Label itemAltPriceTitle = new Label(ITEM_TOTAL_TITLE);
+        TextField itemAltPriceField = new TextField();
+        HBox itemAltPriceHB = new HBox(itemAltPriceTitle,itemAltPriceField);
+
+        // Create item TF VBox
+        VBox itemAltTitleFieldVB = new VBox(itemAltCodeHB,itemAltNameHB,itemAltPriceHB);
+
+        // Create Add/Done buttons and button HBox
+        Button addItemButton = new Button(ADD_ITEM_BUTTON_TITLE);
+        // ADD ITEM HANDLER
+        Button addDoneButton = new Button(DONE_BUTTON_TITLE);
+        addDoneButton.setOnAction(event -> primaryStage.setScene(dataAltScene));
+        HBox addButtonHB = new HBox(addItemButton,addDoneButton);
+
+        // Create add item node
+        VBox addItemVB = new VBox(addItemAltSceneTitle,itemAltTitleFieldVB,addButtonHB);
+
+        // Style nodes
+
+        // Create scene
+        addItemScene = new Scene(addItemVB);
+
+
+        // Delete scene
+        // Create delete item scene title
+        Label deleteItemAltSceneTitle = new Label(DELETE_ITEM_TITLE);
+
+        // Select code from itemComBoxTilePane
+
+        // Preview item from comboBox in title/field HBox nodes
+        itemAltCodeTitle = new Label(ITEM_NAME_TITLE);
+        itemAltCodeField = new TextField();
+        itemAltCodeHB = new HBox(itemAltCodeTitle,itemAltCodeField);
+
+        itemAltNameTitle = new Label(ITEM_PRICE_TITLE);
+        itemAltNameField = new TextField();
+        itemAltNameHB = new HBox(itemAltNameTitle,itemAltNameField);
+
+        itemAltPriceTitle = new Label(ITEM_TOTAL_TITLE);
+        itemAltPriceField = new TextField();
+        itemAltPriceHB = new HBox(itemAltPriceTitle,itemAltPriceField);
+
+        // Create item TF VBox
+        itemAltTitleFieldVB = new VBox(itemAltCodeHB,itemAltNameHB,itemAltPriceHB);
+
+        // Create Delete/Done buttons and button HBox
+        Button deleteItemButton = new Button(DELETE_ITEM_BUTTON_TITLE);
+        // DELETE ITEM HANDLER
+        Button deleteDoneButton = new Button(DONE_BUTTON_TITLE);
+        deleteDoneButton.setOnAction(event -> primaryStage.setScene(dataAltScene));
+        HBox deleteButtonHB = new HBox(deleteItemButton,deleteDoneButton);
+
+        // Create delete item node
+        VBox deleteItemVB = new VBox(deleteItemAltSceneTitle,itemAltComBoxTilePane,itemAltTitleFieldVB,deleteButtonHB);
+
+        // Style nodes
+
+        // Create scene
+        deleteItemScene = new Scene(deleteItemVB);
+
+
+        // Modify scene
+        // Create modify item scene title
+        Label modifyItemAltSceneTitle = new Label(MODIFY_ITEM_TITLE);
+
+        // Select code from itemComBoxTilePane
+
+        // Preview/modify item fields
+        itemAltCodeTitle = new Label(ITEM_NAME_TITLE);
+        itemAltCodeField = new TextField();
+        itemAltCodeHB = new HBox(itemAltCodeTitle,itemAltCodeField);
+
+        itemAltNameTitle = new Label(ITEM_PRICE_TITLE);
+        itemAltNameField = new TextField();
+        itemAltNameHB = new HBox(itemAltNameTitle,itemAltNameField);
+
+        itemAltPriceTitle = new Label(ITEM_TOTAL_TITLE);
+        itemAltPriceField = new TextField();
+        itemAltPriceHB = new HBox(itemAltPriceTitle,itemAltPriceField);
+
+        // Create item TF VBox
+        itemAltTitleFieldVB = new VBox(itemAltCodeHB,itemAltNameHB,itemAltPriceHB);
+
+        // Create Modify/Done buttons and button HBox
+        Button modifyItemButton = new Button(MOD_ITEM_BUTTON_TITLE);
+        // MODIFY ITEM HANDLER
+        Button modifyDoneButton = new Button(DONE_BUTTON_TITLE);
+        modifyDoneButton.setOnAction(event -> primaryStage.setScene(dataAltScene));
+        HBox modifyButtonHB = new HBox(modifyItemButton,modifyDoneButton);
+
+        // Create modify item node
+        VBox modifyItemVB = new VBox(modifyItemAltSceneTitle,itemAltComBoxTilePane,itemAltTitleFieldVB,modifyButtonHB);
+
+        // Style nodes
+
+        // Create scene
+        modifyItemScene = new Scene(modifyItemVB);
 
         // dataAlt scene
-
         // Create dataAlt title node
         Label dataAltLabel = new Label(DATA_ALT_TITLE);
 
         // Create dataAlt button nodes
-        Button addItemButton = new Button(ADD_ITEM_BUTTON_TITLE);
-        Button deleteItemButton = new Button(DELETE_ITEM_BUTTON_TITLE);
-        Button modItemButton = new Button(MOD_ITEM_BUTTON_TITLE);
-        HBox dataAltButtonHB = new HBox(addItemButton,deleteItemButton,modItemButton);
+        Button dataAltAddItemButton = new Button(ADD_ITEM_BUTTON_TITLE);
+        dataAltAddItemButton.setOnAction(e -> primaryStage.setScene(addItemScene));
+        Button dataAltDeleteItemButton = new Button(DELETE_ITEM_BUTTON_TITLE);
+        dataAltDeleteItemButton.setOnAction(event -> primaryStage.setScene(deleteItemScene));
+        Button dataAltModItemButton = new Button(MOD_ITEM_BUTTON_TITLE);
+        dataAltModItemButton.setOnAction(event -> primaryStage.setScene(modifyItemScene));
+        HBox dataAltButtonHB = new HBox(dataAltAddItemButton,dataAltDeleteItemButton,dataAltModItemButton);
 
         // Create done button node
         Button quitButton = new Button(QUIT_BUTTON_TITLE);
 
+        // Create dataAlt node
         VBox dataAltVB = new VBox(dataAltLabel,dataAltButtonHB,quitButton);
 
+        // Style nodes
+
         // Create scene
-        Scene dataAltScene = new Scene(dataAltVB);
+        dataAltScene = new Scene(dataAltVB);
+
 
         // sale scene
 
-        // Create top add border pane
+        // Select item from itemComBoxTilePane
 
-        // Create ComboBox node
-        // Need to redo for Item objects
-        ComboBox<Item> itemIDComBox = new ComboBox<>();
+        // Create item title HBox nodes
+        Label itemSaleCodeTitle = new Label(ITEM_NAME_TITLE);
+        TextField itemSaleCodeField = new TextField();
+        HBox itemSaleCodeHB = new HBox(itemSaleCodeTitle,itemSaleCodeField);
 
-        // Create item title VBox and nodes
-        Label itemNameLabel = new Label(ITEM_NAME_TITLE);
-        Label itemPriceLabel = new Label(ITEM_PRICE_TITLE);
-        Label itemQuantityLabel = new Label(ITEM_QUANTITY_TITLE);
-        Label itemTotalLabel = new Label(ITEM_TOTAL_TITLE);
-        VBox itemTitlesVB = new VBox(itemNameLabel,itemPriceLabel,itemQuantityLabel,itemTotalLabel);
+        Label itemSaleNameTitle = new Label(ITEM_PRICE_TITLE);
+        TextField itemSaleNameField = new TextField();
+        HBox itemSaleNameHB = new HBox(itemSaleNameTitle,itemSaleNameField);
 
-        // Create item field VBox and nodes
-        Label itemNameFieldLabel = new Label(ITEM_NAME_FIELD_DEFAULT);
-        Label itemPriceFieldLabel = new Label(ITEM_PRICE_FIELD_DEFAULT);
-        TextField itemQuantityFieldTF = new TextField();
-        Label itemTotalFieldLabel = new Label(ITEM_TOTAL_FIELD_DEFAULT);
-        VBox itemFieldsVB = new VBox(itemNameFieldLabel,itemPriceFieldLabel,itemQuantityFieldTF,itemTotalFieldLabel);
+        Label itemSalePriceTitle = new Label(ITEM_TOTAL_TITLE);
+        TextField itemSalePriceField = new TextField();
+        HBox itemSalePriceHB = new HBox(itemSalePriceTitle,itemSalePriceField);
 
-        // Create top center pane item HBox with item VBoxes
-        HBox itemHB = new HBox(itemTitlesVB,itemFieldsVB);
+        Label itemSaleQuantityTitle = new Label(ITEM_QUANTITY_TITLE);
+        TextField itemSaleQuantityField = new TextField();
+        HBox itemSaleQuantityHB = new HBox(itemSaleQuantityTitle,itemSaleQuantityField);
+
+        // Create item TF VBox
+        VBox itemSaleTitleFieldVB = new VBox(itemSaleCodeHB,itemSaleNameHB,itemSalePriceHB,itemSaleQuantityHB);
+
+        // Create update event for sale field nodes
+        itemComBox.setOnAction(event -> {
+            itemSaleCodeField.setText(itemComBox.getValue().getItemCode());
+            itemSaleNameField.setText(itemComBox.getValue().getItemName());
+            itemSalePriceField.setText(itemComBox.getValue().getItemPriceString());
+        });
 
         // Create add button to top of sale
         Button addButton = new Button(ADD_BUTTON_TITLE);
+        // ADD HANDLER
 
+        // Create sale node
         BorderPane saleTopBP = new BorderPane();
-        saleTopBP.setTop(itemIDComBox);
-        saleTopBP.setCenter(itemHB);
+        saleTopBP.setTop(itemSaleComBoxTilePane);
+        saleTopBP.setCenter(itemSaleTitleFieldVB);
         saleTopBP.setBottom(addButton);
 
         // Create middle checkout Vbox
-
         // Create receipt Label node
-        Label receiptLabel = new Label(RECEIPT_TEXTFIELD);
+        TextField receiptTextField = new TextField(RECEIPT_TEXTFIELD);
 
         // Create subtotal title VBox and nodes
         Label subtotalLabel = new Label(SUBTOTAL_TITLE);
@@ -130,11 +290,14 @@ public class HW3_Peters extends Application {
         Label changeFieldLabel = new Label(CHANGE_FIELD_DEFAULT);
         VBox subtotalFieldVB = new VBox(subtotalFieldLabel,subtotalTaxFieldLabel,tenderTF,changeFieldLabel);
 
+        // Create subtotal HBox
+        HBox subtotalHB = new HBox(subtotalTitleVB,subtotalFieldVB);
+
         // Create checkout button node
         Button checkoutButton = new Button(CHECKOUT_BUTTON_TITLE);
 
-        // Create subtotal VBox
-        VBox checkoutVB = new VBox(receiptLabel,subtotalTitleVB,subtotalFieldVB,checkoutButton);
+        // Create checkoutVBox
+        VBox checkoutVB = new VBox(receiptTextField,subtotalHB,checkoutButton);
 
         // Create bottom button Hbox
         Button doneButton = new Button(DONE_BUTTON_TITLE);
@@ -146,8 +309,11 @@ public class HW3_Peters extends Application {
         saleBP.setCenter(checkoutVB);
         saleBP.setBottom(doneButton);
 
+        // Style nodes
+
         // Create then set SaleScene
-        Scene saleScene = new Scene(saleBP);
+        saleScene = new Scene(saleBP);
+
 
         // Main scene
         // Create main screen scene nodes
@@ -158,14 +324,17 @@ public class HW3_Peters extends Application {
 
         Label eodTotalLabel = new Label(EOD_TOTAL_TITLE + eodTotalPrice);
 
-        // Create main border pane and set nodes to positions. Title to top, button to center, label to bottom
+        // Create main node
         BorderPane mainBP = new BorderPane();
         mainBP.setTop(mainTitleLabel);
         mainBP.setCenter(newSaleButton);
         mainBP.setBottom(eodTotalLabel);
 
+        // Style nodes
+
         // Create then set mainScene
-        Scene mainScene = new Scene(mainBP);
+        mainScene = new Scene(mainBP);
+
 
         // Set title and scene then show stage
         primaryStage.setTitle(MAIN_TITLE);
@@ -175,5 +344,55 @@ public class HW3_Peters extends Application {
 
     public static void main(String[] args) {
         launch();
+    }
+
+    private static void updateItemList() {
+        // Update itemObservableList and itemComBox
+    }
+
+    private static void Initializing() {
+        {
+            itemArrayList = new ArrayList<>();
+            File fileRef;
+            String fileLine;
+            String[] splitFileImport;
+            Scanner fileScanner;
+
+            // input from item.txt
+            try {
+                // Create file
+                fileRef = new File(FILE_NAME_KEY);
+
+                // Set up scanner
+                fileScanner = new Scanner(fileRef);
+
+                // Scan in input and split by new line commas
+                fileLine = fileScanner.nextLine();
+                splitFileImport = fileLine.split(Pattern.quote(","));
+
+                // For loop to index and input file data into itemArray
+                // Input code, name, price into Items objects in the array, advance index by 1, split index by 4
+                for (int index = 0, splitIndex = 0; index < 10; index+=1, splitIndex+=3) {
+
+
+                    // Create by index in array and imported from split input array
+                    itemArrayList.add(new Item(splitFileImport[splitIndex],
+                            String.valueOf(splitFileImport[splitIndex+1]),
+                            Double.parseDouble(splitFileImport[splitIndex+2])));
+                }
+
+                // Close scanner
+                fileScanner.close();
+
+            } catch (Exception exception) {
+                // File input failed
+                exception.printStackTrace();
+            }
+        }
+        // initialize itemObservableList
+        itemObservableList = FXCollections.observableList(itemArrayList);
+
+        // initialize itemComBox
+        itemComBox = new ComboBox<>(itemObservableList);
     }
 }
--- src/Item.java
+++ src/Item.java
@@ -1,3 +1,133 @@
+/**
+ * @author Anthony Peters
+ *
+ * Holds Items codes, names, prices, and taxable state
+ */
 public class Item {
 
+    // Global vars
+    private String itemCode;
+    private String itemName;
+    private double itemPrice;
+    private boolean itemTaxable;
+
+    /**
+     * Non-Aug. Constructor
+     *
+     * Sets fields to placeholder values meant to be overriden
+     */
+    Item(){
+        itemCode = "";
+        itemName = "";
+        itemPrice = 0.00;
+        itemTaxable = false;
+    }
+
+    /**
+     * Aug. Constructor
+     *
+     * @param importItemCode      String, A or B + [###]
+     * @param importItemName      String, title of item
+     * @param importPrice         Double, price with 2 Significant figures
+     */
+    Item(String importItemCode, String importItemName, double importPrice){
+        itemCode = importItemCode;
+        itemName = importItemName;
+        itemPrice = importPrice;
+        itemTaxable = this.isTaxable(importItemCode);
+    }
+
+    /**
+     * Returns item taxable boolean
+     *
+     * @param importItemCode, String
+     * @return boolean, itemTaxable
+     */
+    private boolean isTaxable(String importItemCode) {
+        // returns taxable state
+        return importItemCode.charAt(0) == 'A';
+    }
+
+    public String getItemPriceString() {
+        return String.valueOf(this.itemPrice);
+    }
+
+    /**
+     * Returns item code
+     *
+     * @return String, itemCode
+     */
+    public String getItemCode() {
+        return this.itemCode;
+    }
+
+    /**
+     * Returns item taxable boolean
+     *
+     * @return boolean, itemTaxable
+     */
+    public boolean getItemTaxable() {
+        return this.itemTaxable;
+    }
+
+    /**
+     * Returns item name
+     *
+     * @return String, itemName
+     */
+    public String getItemName() {
+        return this.itemName;
+    }
+
+    /**
+     * Returns item price
+     *
+     * @return double, itemPrice
+     */
+    public double getItemPrice() {
+        return this.itemPrice;
+    }
+
+    /**
+     * Sets item code
+     *
+     * @param importItemCode, String itemCode
+     */
+    public void setItemCode(String importItemCode) {
+        // Set item code
+        this.itemCode = importItemCode;
+    }
+
+    /**
+     * Sets item taxable flag
+     *
+     * @param importItemCode, String itemCode
+     */
+    public void setItemTaxable(String importItemCode) {
+        // Check taxable status
+        this.itemTaxable = this.isTaxable(importItemCode);
+    }
+
+    /**
+     * Sets item name
+     *
+     * @param importItemName, itemName
+     */
+    public void setItemName(String importItemName) {
+        this.itemName = importItemName;
+    }
+
+    /**
+     * Sets item price
+     *
+     * @param importPrice, double itemPrice
+     */
+    public void setPrice(double importPrice) {
+        this.itemPrice = importPrice;
+    }
+
+    @Override
+    public String toString() {
+        return itemName + "," + itemCode;
+    }
 }
